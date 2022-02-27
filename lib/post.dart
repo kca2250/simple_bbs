1bcf394ca1d9c5d8971c6e24593336fbedd699de
kca2250 <2250katok@gmail.com>
2022-02-27 22:40:10 +0900

    ✨: _onSubmit関数で入力完了したら投稿する機能を実装

--- lib/post.dart
+++ lib/post.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({Key? key}) : super(key: key);
@@ -17,22 +19,32 @@ class _PostPageState extends State<PostPage> {
       appBar: AppBar(
         title: const Text("投稿画面"),
       ),
-      body: Center(
-        child: TextField(
-          controller: _textEditingController,
-          enabled: true,
-          maxLength: 50,
-          maxLengthEnforcement: MaxLengthEnforcement.enforced,// 入力上限になったときに文字入力を抑制するか
-          style: const TextStyle(color: Colors.black),
-          obscureText: false,
-          maxLines: 1,
-          decoration: const InputDecoration(
-            icon: Icon(Icons.speaker_notes),
-            hintText: "投稿内容を記載します",
-            labelText: "内容 * "
-          ),
+      body: TextField(
+        controller: _textEditingController,
+        onSubmitted: _onSubmit,
+        enabled: true,
+        maxLength: 50,
+        maxLengthEnforcement: MaxLengthEnforcement.enforced,// 入力上限になったときに文字入力を抑制するか
+        style: const TextStyle(color: Colors.black),
+        obscureText: false,
+        maxLines: 1,
+        decoration: const InputDecoration(
+          icon: Icon(Icons.speaker_notes),
+          hintText: "投稿内容を記載します",
+          labelText: "内容 * "
         ),
       )
     );
+  }
+
+  _onSubmit(String content) {
+    CollectionReference posts = FirebaseFirestore.instance.collection("posts");
+    posts.add({
+      "content": content,
+      "createdAt": Timestamp.now(),
+    });
+
+    _textEditingController.clear();
+    Navigator.pushNamed(context, "/");
   }
 }
